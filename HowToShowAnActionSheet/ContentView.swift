47511f40620a06a6059e9e7f90825a23f8df344d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-02 17:16:14 +0300

    How to show an action sheet

--- HowToShowAnActionSheet/ContentView.swift
+++ HowToShowAnActionSheet/ContentView.swift
@@ -9,8 +9,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingSheet = false
+
     var body: some View {
-        Text("Hello World")
+        Button(action: {
+            self.showingSheet = true
+        }) {
+            Text("Show Action Sheet")
+        }
+        .actionSheet(isPresented: $showingSheet) {
+            ActionSheet(title: Text("What do you want to do?"), message: Text("There's only one choice..."), buttons: [.default(Text("Dismiss Action Sheet"))])
+        }
     }
 }
 
